5c0b54e8ced4f431c97c6907a4daf5dd625d2bc2
Jola Klodowska <jola.klodowska@wp.pl>
2022-04-21 18:14:29 +0200

    Changing initialState

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,16 @@
-import styles from './Card.module.scss'
+import styles from './Card.module.scss';
+import clsx from 'clsx';
 
 const Card = props => {
+
+console.log(props.clicked);
+
+const handleSubmit = e => {
+    e.preventDefault();
+}
+
     return (
-        <li className={styles.cards}>
+        <li onClick={handleSubmit} className={clsx(styles.cards, {[styles.active] : props.clicked === true})}>
             <div className={styles.name}>
                 <h3 className={styles.mountain}>{props.name}</h3>
                 <h4 className={styles.height}>{props.height}</h4>

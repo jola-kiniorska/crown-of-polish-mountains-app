052cc322a6eba95e905aa697b108134f5a2b86d6
Jola Klodowska <jola.klodowska@wp.pl>
2022-04-27 11:24:34 +0200

    Finish reached mountains counter

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,31 +2,38 @@ import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addClickedMountains } from '../../redux/store';
+import { addClickedMountains, removeClickedMountains } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import { getClickedMountains } from '../../redux/store';
 
 const Card = props => {
 
-const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-const [clicked, setClicked] = useState(props.clicked);
+    const [clicked, setClicked] = useState(props.clicked);
+    const counter = useSelector(getClickedMountains)
 
-const handleSubmit = e => {
-    e.preventDefault();
-    setClicked(clicked => !clicked);
-    const mountainId = props.id;
-    dispatch(addClickedMountains({ mountainId }));
-}
+    const handleSubmit = e => {
+        e.preventDefault();
+        setClicked(clicked => !clicked);
+        const mountainId = props.id;
+        if (!counter.includes(mountainId)) {
+            dispatch(addClickedMountains({ mountainId }));
+        } else {
+            dispatch(removeClickedMountains({ mountainId }));
+        }
+    }
 
-    if(clicked) 
-    return (
-        <li onClick={handleSubmit} className={clsx(styles.cards, styles.clicked)}>
-            <div className={styles.name}>
-                <h3 className={styles.nameClicked}>{props.name}</h3>
-                <h3 className={styles.reached}>REACHED!</h3>
-                <h3 className={styles.nameClicked}>{props.height}</h3>
-            </div>
-        </li>
-    );
+    if (clicked)
+        return (
+            <li onClick={handleSubmit} className={clsx(styles.cards, styles.clicked)}>
+                <div className={styles.name}>
+                    <h3 className={styles.nameClicked}>{props.name}</h3>
+                    <h3 className={styles.reached}>REACHED!</h3>
+                    <h3 className={styles.nameClicked}>{props.height}</h3>
+                </div>
+            </li>
+        );
     return (
         <li onClick={handleSubmit} className={styles.cards}>
             <div className={styles.name}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,13 +8,19 @@ export const getClickedMountains = state => state.clickedMountains;
 // actions
 const createActionName = actionName => `app/lists/${actionName}`;
 const ADD_CLICKED_MOUNTAINS = createActionName('GET_CLICKED_MOUNTAINS');
+const REMOVE_CLICKED_MOUNTAINS = createActionName('REMOVE_CLICKED_MOUNTAINS');
 
 // action creators
 export const addClickedMountains = payload => ({ type: ADD_CLICKED_MOUNTAINS, payload})
+export const removeClickedMountains = payload => ({ type: REMOVE_CLICKED_MOUNTAINS, payload})
 const reducer = (statePart = {}, action) => {
     switch (action.type) {
         case ADD_CLICKED_MOUNTAINS:
+            debugger;
             return { ...statePart, clickedMountains: [...statePart.clickedMountains, action.payload.mountainId]};
+        case REMOVE_CLICKED_MOUNTAINS:
+            debugger;
+            return {...statePart, clickedMountains: statePart.clickedMountains.filter(item => item !== action.payload.mountainId)};
         default:
             return statePart;
     }
